db393bd66c460dba0fac809e78850b9d3c86ac3a
Caden A <canton@uoguelph.ca>
2025-01-26 02:00:41 -0500

    fixed functionality with github io

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar(){
     return (
         <div class="topnav">
           <h1>SIMONSITE</h1>
-          <NavLink exact to="/" className="nav-link" activeClassName="active">Home</NavLink>
+          <NavLink exact to="/home" className="nav-link" activeClassName="active">Home</NavLink>
           <NavLink to="/blog" className="nav-link" activeClassName="active">Blog</NavLink>
           <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
         </div>
