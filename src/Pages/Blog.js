2b8d43c0627ecc3bbcfcb510709082940a560c80
ysogg <ajb.personal@hotmail.com>
2025-02-06 12:56:52 -0500

    Setup firebase + read DB on Blog page

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,7 +1,34 @@
 import React from "react";
 import Navbar from '../Components/Navbar';
 import '../styles/global.css';
+import { async } from '@firebase/util'
+import { db } from '../index.js';
+
+import { 
+    collection, 
+    doc,
+    getDocs,
+    query,
+    limit, 
+    where,
+    getFirestore 
+} from "firebase/firestore";
+
+
+async function getAllPosts() {
+    const postsQuery = query(
+        collection(db, 'posts'),
+        limit(10)
+    );
+
+    const querySnapshot = await getDocs(postsQuery);
+    querySnapshot.forEach((post) => {
+        console.log(`Post Name: ${post.id} | Post Data: ${JSON.stringify(post.data())}`);
+    });
+}
+
 function Blog(){
+    getAllPosts();
     return(
         <Navbar></Navbar>
         
