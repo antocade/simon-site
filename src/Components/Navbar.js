f463b04d9f938055d90adfd14e3331307a1d7ff3
ysogg <ajb.personal@hotmail.com>
2025-04-21 15:46:26 -0400

    Layout outline
    Need to fix navbar component not being rendered after selection

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink } from 'react-router-dom'; 
+import { NavLink, useLocation } from 'react-router-dom'; 
 import { NavBtn } from "./Buttons";
 import { auth } from "../index.js";
 import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
@@ -106,6 +106,8 @@ function Navbar(){
         }
     }
 
+    const location = useLocation()
+
     useEffect(() => {
         window.addEventListener("keydown", keyboardListener);
 
@@ -116,14 +118,22 @@ function Navbar(){
                 setLoginStatus(false)
             }
         });
-    });
+
+        if (location.hash) {
+            const element = document.querySelector(location.hash)
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth' })
+            }
+        }
+    }, [location.hash]);
 
     return (
         <div class="topnav">
             <h1>SIMONSITE TEST</h1>
             <NavLink exact to="/simon-site" className="nav-link" activeClassName="active">Home</NavLink>
-            <NavLink to="/blog" className="nav-link" activeClassName="active">Blog</NavLink>
-            <NavLink to="/about" className="nav-link" activeClassName="active">About</NavLink>
+            {/* <NavLink to="/blog" className="nav-link" activeClassName="active">Blog</NavLink> */}
+            <NavLink to="#sect2" className="nav-link" activeClassName="active">Sect2</NavLink>
+            <NavLink to="#sect3" className="nav-link" activeClassName="active">Sect3</NavLink>
             <Conditional isLoggedIn={loginStatus}/>
             <LoginModal isOpen={isOpen}>
                 <LoginContent>
--- src/Pages/App.js
+++ src/Pages/App.js
@@ -24,23 +24,33 @@ function App() {
     const timeout = setTimeout(() => {
       setFadeComplete(true);}, 100);
     return () => clearTimeout(timeout);}, []);
+
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <Navbar></Navbar>
-        </header>
-        <div className={`fade-overlay ${fadeComplete ? 'hidden' : ''}`}></div>
-        <Canvas dpr={[1,2]} shadows camera={{position: [0,0,8], fov:55}} style={{"position": "fixed"}}>
-          <color attach="background" args={["#101010"]}/>
-          <pointLight position={[0, 5, 5]} intensity={20.5} />
-          <directionalLight position={[0, 10, 10]} intensity={2} castShadow />
-          <PresentationControls speed={1.5} global zoom={1}>
-                <Model1 scale={50} position={[-18.4, -13.2, 10]} rotation-y={Math.PI * -0.21}/>
-              <Model2 scale={0.049} position={[0, -2, -2]}/>
-          </PresentationControls>
-        </Canvas>
-      
+      </header>
+
+      <div class="parent row">
+        <section class="sect" id="sect1">
+          <div className={`fade-overlay ${fadeComplete ? 'hidden' : ''}`}></div>
+            <Canvas dpr={[1,2]} shadows camera={{position: [0,0,8], fov:55}}> 
+              <color attach="background" args={["#101010"]}/>
+              <pointLight position={[0, 5, 5]} intensity={20.5} />
+              <directionalLight position={[0, 10, 10]} intensity={2} castShadow />
+              <PresentationControls speed={1.5} global zoom={1}>
+                    <Model1 scale={50} position={[-18.4, -13.2, 10]} rotation-y={Math.PI * -0.21}/>
+                  <Model2 scale={0.049} position={[0, -2, -2]}/>
+              </PresentationControls>
+            </Canvas>
+        </section>
+        <section class="sect" id="sect2"></section>
+        <section class="sect" id="sect3"></section>
+      </div>
+
     </div>
+    </>
   );
 }
 
